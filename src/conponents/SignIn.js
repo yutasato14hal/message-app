d972773e96e327306cd267c02516c33948808d27
佐藤優太 <83742609+yutasato14hal@users.noreply.github.com>
2023-06-13 14:03:28 +0900

    Update SignIn.js

--- src/conponents/SignIn.js
+++ src/conponents/SignIn.js
@@ -1,18 +1,18 @@
-import { Button } from "@mui/material";
 import React from "react";
 import firebase from "firebase/compat/app";
 import { auth } from "../firebase.js";
+import Button from "@mui/material/Button";
 
 function SignIn() {
-  function signInWhithGoogle() {
+  function signInWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
   }
   return (
     <div className="header">
-      <Button style={{fontSize:"15px", fontWeight:"bold", color:"gray", background:"#fff"}} onClick={signInWhithGoogle}>グーグルでログインする</Button>
+      <Button onClick={signInWithGoogle} style={{fontSize:"15px", fontWeight:"bold", color:"gray", background:"#fff"}} >グーグルでログインする</Button>
     </div>
   );
 }
 
-export default SignIn;
+export default SignIn; 

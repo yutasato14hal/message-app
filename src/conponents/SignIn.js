ff1b425d3a20f8d9e977c724e5675b5601d54130
佐藤優太 <83742609+yutasato14hal@users.noreply.github.com>
2023-06-13 14:10:19 +0900

    Update SignIn.js

--- src/conponents/SignIn.js
+++ src/conponents/SignIn.js
@@ -11,6 +11,7 @@ function SignIn() {
   return (
     <div className="header">
       <Button onClick={signInWithGoogle} style={{fontSize:"15px", fontWeight:"bold", color:"gray", background:"#fff"}} >グーグルでログインする</Button>
+   <div>本番環境で動作しなくなったため、調整中です。</div>
     </div>
   );
 }
